cf4c96b1327626c1a47b24080356b0f368bb7950
Felix Lie <felixlie@Felixs-MacBook-Air.local>
2026-09-14 08:14:02 +0700

    robofriend with create react app

---
+++ src/containers/App.js
@@ -0,0 +1,50 @@
+import {Component} from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/scroll';
+import './App.css';
+
+
+
+class App extends Component {
+    // state hold a data
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users})
+        )
+    }
+    // event handlers when search box is filled
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+    }
+
+    render() {
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        })
+        return !robots.length ? 
+        <h1>Loading</h1> :
+        (
+            <div className='tc'>
+                <h1 className="f1">RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <Scroll>
+                    <CardList robots={filterRobots}/>
+                </Scroll>
+            </div>
+            
+        )
+    }
+}
+
+export default App;
